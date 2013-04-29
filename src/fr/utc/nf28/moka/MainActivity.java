f7a90e9d2f2b5ed412613d6bd347d5ddf54fc0e8
Thomas <thomas.keunebroek@gmail.com>
2013-04-29 14:23:44 +0200

    Implement basic MainActivity navigation, using a ViewPager

--- src/fr/utc/nf28/moka/MainActivity.java
+++ src/fr/utc/nf28/moka/MainActivity.java
@@ -1,12 +1,91 @@
 package fr.utc.nf28.moka;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import fr.utc.nf28.moka.ui.CurrentElementListFragment;
+import fr.utc.nf28.moka.ui.ElementListFragment;
 
-public class MainActivity extends Activity {
+public class MainActivity extends SherlockFragmentActivity implements ActionBar.TabListener {
+	/**
+	 * The {@link ViewPager} that will host the section contents.
+	 */
+	private ViewPager mViewPager;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.main);
+
+		// ActionBar setup
+		final ActionBar actionBar = getSupportActionBar();
+		actionBar.setHomeButtonEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		// ViewPager setup
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+			@Override
+			public void onPageSelected(int position) {
+				actionBar.setSelectedNavigationItem(position);
+			}
+		});
+		mViewPager.setAdapter(new SectionsPagerAdapter(getSupportFragmentManager()));
+
+		// We add our tabs
+		actionBar.addTab(actionBar.newTab()
+				.setText(getString(R.string.tab_title_create))
+				.setTabListener(this));
+
+		actionBar.addTab(actionBar.newTab()
+				.setText(getString(R.string.tab_title_current))
+				.setTabListener(this));
+
+	}
+
+	@Override
+	public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+		mViewPager.setCurrentItem(tab.getPosition());
+	}
+
+	@Override
+	public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+	}
+
+	@Override
+	public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+	}
+
+	/**
+	 * A {@link android.support.v4.app.FragmentPagerAdapter} that returns a fragment corresponding to
+	 * one of the sections/tabs/pages.
+	 */
+	private static class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+		public SectionsPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			// getItem is called to instantiate the fragment for the given page.
+			switch (position) {
+				case 0:
+					return new ElementListFragment();
+				default:
+					return new CurrentElementListFragment();
+			}
+		}
+
+		@Override
+		public int getCount() {
+			return 2;
+		}
 	}
 }
---
+++ src/fr/utc/nf28/moka/ui/CurrentElementListFragment.java
@@ -0,0 +1,27 @@
+package fr.utc.nf28.moka.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.actionbarsherlock.app.SherlockFragment;
+import fr.utc.nf28.moka.R;
+
+public class CurrentElementListFragment extends SherlockFragment {
+	public CurrentElementListFragment() {
+	}
+
+	// Fragment lifecycle management
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Fragment configuration
+		setHasOptionsMenu(true);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_current_element_list, container, false);
+	}
+}
