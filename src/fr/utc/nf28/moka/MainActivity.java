10fd374a21e7216a2fa7cfc7f096d0a36aa6cdb2
Thomas <thomas.keunebroek@gmail.com>
2013-04-29 13:06:11 +0200

    Initial commit (basic application)

---
+++ src/fr/utc/nf28/moka/MainActivity.java
@@ -0,0 +1,12 @@
+package fr.utc.nf28.moka;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
