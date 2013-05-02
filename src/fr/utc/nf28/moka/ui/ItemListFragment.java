c37c604febf26b75350d6153471a1e61e2185b0b
Thomas <thomas.keunebroek@gmail.com>
2013-05-02 17:21:33 +0200

    Add the Spinner that allows switching between item classes
    Note: the selected class in retained using SharedPreferences

--- src/fr/utc/nf28/moka/ui/ItemListFragment.java
+++ src/fr/utc/nf28/moka/ui/ItemListFragment.java
@@ -1,12 +1,14 @@
 package fr.utc.nf28.moka.ui;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.GridView;
+import android.widget.*;
 import com.actionbarsherlock.app.SherlockFragment;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
@@ -22,8 +24,9 @@ import java.util.List;
 import static fr.utc.nf28.moka.util.LogUtils.makeLogTag;
 
 public class ItemListFragment extends SherlockFragment implements AdapterView.OnItemClickListener,
-		SearchView.OnQueryTextListener, MenuItem.OnActionExpandListener {
+		SearchView.OnQueryTextListener, MenuItem.OnActionExpandListener, AdapterView.OnItemSelectedListener {
 	private static final String TAG = makeLogTag(ItemListFragment.class);
+	private static final String PERSISTENT_LAST_CLASS = "Moka_LastClass";
 	/**
 	 * A dummy implementation of the {@link Callbacks} interface that does
 	 * nothing. Used only when this fragment is not attached to an activity.
@@ -36,6 +39,8 @@ public class ItemListFragment extends SherlockFragment implements AdapterView.On
 	private final List<BaseItem> items = new ArrayList<BaseItem>(10);
 	private ItemAdapter mAdapter;
 	private Callbacks mCallbacks = sDummyCallbacks;
+	private SharedPreferences mPrefs;
+	private Spinner mSpinner;
 
 	public ItemListFragment() {
 	}
@@ -70,6 +75,13 @@ public class ItemListFragment extends SherlockFragment implements AdapterView.On
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		final View rootView = inflater.inflate(R.layout.fragment_item_list, container, false);
 
+		mSpinner = (Spinner) rootView.findViewById(R.id.spinner);
+		mSpinner.setOnItemSelectedListener(this);
+		final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getSherlockActivity(),
+				R.array.item_classes, R.layout.simple_spinner_item_bigger);
+		adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item_bigger);
+		mSpinner.setAdapter(adapter);
+
 		final GridView gridView = (GridView) rootView.findViewById(R.id.grid);
 		gridView.setOnItemClickListener(this);
 		gridView.setEmptyView(rootView.findViewById(android.R.id.empty));
@@ -79,6 +91,14 @@ public class ItemListFragment extends SherlockFragment implements AdapterView.On
 		gridView.setAdapter(mAdapter);
 
 		return rootView;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+		mPrefs = PreferenceManager.getDefaultSharedPreferences(getSherlockActivity());
+		mSpinner.setSelection(loadLastClassPreference());
 	}
 
 	@Override
@@ -109,6 +129,23 @@ public class ItemListFragment extends SherlockFragment implements AdapterView.On
 		// There might be a better solution.
 	}
 
+	private void saveLastClassPreference(int position) {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+			mPrefs.edit().putInt(PERSISTENT_LAST_CLASS, position).apply();
+		} else {
+			mPrefs.edit().putInt(PERSISTENT_LAST_CLASS, position).commit();
+		}
+	}
+
+	private int loadLastClassPreference() {
+		try {
+			return mPrefs.getInt(PERSISTENT_LAST_CLASS, mSpinner.getCount() - 1);
+		} catch (IllegalArgumentException e) {
+			// Preference is corrupt?
+			return 0;
+		}
+	}
+
 	@Override
 	public boolean onQueryTextSubmit(String query) {
 		return false;
@@ -129,6 +166,17 @@ public class ItemListFragment extends SherlockFragment implements AdapterView.On
 	public boolean onMenuItemActionCollapse(MenuItem item) {
 		mAdapter.getFilter().filter(null);
 		return true;
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+		saveLastClassPreference(position);
+		Toast.makeText(getSherlockActivity(), parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+		// TODO: apply corresponding filter
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> parent) {
 	}
 
 	/**
