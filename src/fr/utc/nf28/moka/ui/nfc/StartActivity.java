32c670b50c62ff9a996c07349082e89e6c981176
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-05-03 09:22:19 +0200

    Create Start Activity and Add Start View

---
+++ src/fr/utc/nf28/moka/ui/nfc/StartActivity.java
@@ -0,0 +1,17 @@
+package fr.utc.nf28.moka.ui.nfc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Hi-Bro
+ * Date: 03/05/13
+ * Time: 08:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class StartActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
