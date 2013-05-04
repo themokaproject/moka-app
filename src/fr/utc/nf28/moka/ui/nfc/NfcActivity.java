2e2b55baf78304024011a883044ce41bd7d223dd
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-05-04 17:26:03 +0200

    Skip button starts MainActivity. Only for dev periode

--- src/fr/utc/nf28/moka/ui/nfc/NfcActivity.java
+++ src/fr/utc/nf28/moka/ui/nfc/NfcActivity.java
@@ -1,8 +1,13 @@
 package fr.utc.nf28.moka.ui.nfc;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import fr.utc.nf28.moka.MainActivity;
 import fr.utc.nf28.moka.R;
+import static fr.utc.nf28.moka.util.LogUtils.makeLogTag;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,8 +17,20 @@ import fr.utc.nf28.moka.R;
  * To change this template use File | Settings | File Templates.
  */
 public class NfcActivity extends Activity {
+
+    private static final String TAG = makeLogTag(NfcActivity.class);
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nfc);
+
+        final Button buttonSkip = (Button)findViewById(R.id.skip);
+        buttonSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(NfcActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
