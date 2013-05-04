2fab76b7161d95df7af735c7b7dab4815be8b645
Thomas <thomas.keunebroek@gmail.com>
2013-05-05 00:30:19 +0200

    Simplify code, remove useless comment and rename layout name

--- src/fr/utc/nf28/moka/ui/nfc/NfcActivity.java
+++ src/fr/utc/nf28/moka/ui/nfc/NfcActivity.java
@@ -4,33 +4,22 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import fr.utc.nf28.moka.MainActivity;
 import fr.utc.nf28.moka.R;
 
 import static fr.utc.nf28.moka.util.LogUtils.makeLogTag;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Hi-Bro
- * Date: 03/05/13
- * Time: 08:29
- * To change this template use File | Settings | File Templates.
- */
 public class NfcActivity extends Activity {
-
 	private static final String TAG = makeLogTag(NfcActivity.class);
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.nfc);
+		setContentView(R.layout.nfc_activity);
 
-		final Button buttonSkip = (Button) findViewById(R.id.skip);
-		buttonSkip.setOnClickListener(new View.OnClickListener() {
+		findViewById(R.id.skip).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				Intent i = new Intent(NfcActivity.this, MainActivity.class);
-				startActivity(i);
+				startActivity(new Intent(NfcActivity.this, MainActivity.class));
 				finish();
 			}
 		});
