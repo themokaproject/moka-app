3f92154279636fff68fb2cb4d0685a2a7344fe8e
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-05-15 11:34:16 +0200

    Create Manual configuration activity

---
+++ src/ManualConfigurationActivity.java
@@ -0,0 +1,15 @@
+import android.app.Activity;
+import android.os.Bundle;
+import fr.utc.nf28.moka.util.LogUtils;
+
+public class ManualConfigurationActivity extends Activity {
+	/**
+	 * Tag for log cat
+	 */
+	private static  final String TAG = LogUtils.makeLogTag(ManualConfigurationActivity.class);
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
