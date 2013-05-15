ad83cec70700908847e5b7bc54f5aff12dab82ca
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-05-15 19:12:05 +0200

    add nfc detection for writting tag

--- src/fr/utc/nf28/moka/ManualConfigurationActivity.java
+++ src/fr/utc/nf28/moka/ManualConfigurationActivity.java
@@ -1,8 +1,11 @@
 package fr.utc.nf28.moka;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
@@ -23,6 +26,11 @@ public class ManualConfigurationActivity extends Activity {
 	public static final String KEY_PREF_IP = "preference_jade_ip";
 	public static final String KEY_PREF_PORT = "preference_jade_port";
 
+	/**
+	 * use to available nfc
+	 */
+	private NfcAdapter mNfcAdapter;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,12 +44,54 @@ public class ManualConfigurationActivity extends Activity {
 			public void onClick(View view) {
 				SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(ManualConfigurationActivity.this);
 				Intent i = new Intent(ManualConfigurationActivity.this, DeviceConfigurationActivity.class);
-				i.putExtra(DeviceConfigurationActivity.EXTRA_SSID,sharedPref.getString(KEY_PREF_SSID,""));
-				i.putExtra(DeviceConfigurationActivity.EXTRA_PWD,sharedPref.getString(KEY_PREF_PWD,""));
-				i.putExtra(DeviceConfigurationActivity.EXTRA_IP,sharedPref.getString(KEY_PREF_IP,""));
-				i.putExtra(DeviceConfigurationActivity.EXTRA_PORT,sharedPref.getString(KEY_PREF_PORT,""));
+				i.putExtra(DeviceConfigurationActivity.EXTRA_SSID, sharedPref.getString(KEY_PREF_SSID, ""));
+				i.putExtra(DeviceConfigurationActivity.EXTRA_PWD, sharedPref.getString(KEY_PREF_PWD, ""));
+				i.putExtra(DeviceConfigurationActivity.EXTRA_IP, sharedPref.getString(KEY_PREF_IP, ""));
+				i.putExtra(DeviceConfigurationActivity.EXTRA_PORT, sharedPref.getString(KEY_PREF_PORT, ""));
 				startActivity(i);
 			}
 		});
+
+		mNfcAdapter = NfcAdapter.getDefaultAdapter(getApplicationContext());
+		if (mNfcAdapter == null) {
+			findViewById(R.id.write).setClickable(false);
+		} else {
+			findViewById(R.id.write).setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					enableNfcDiscovering();
+				}
+			});
+		}
+	}
+
+	/**
+	 * let the activity to claim priority on tag_discover event when
+	 * the activity is displayed
+	 */
+
+	private void enableNfcDiscovering() {
+		if (mNfcAdapter != null) {
+			PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+					new Intent(this, this.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+			IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+			mNfcAdapter.enableForegroundDispatch(this, pendingIntent, new IntentFilter[]{tagDetected}, null);
+		}
+	}
+
+	/**
+	 * restore nfc priority
+	 */
+	private void disableNfcDiscovering() {
+		if (mNfcAdapter != null) {
+			mNfcAdapter.disableForegroundDispatch(this);
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		disableNfcDiscovering();
 	}
 }
+
