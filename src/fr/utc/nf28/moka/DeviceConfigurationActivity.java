06ae2b3fbb1d01f3c36a6cea21b4f4bb6ab1858f
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-05-16 09:25:45 +0200

    Implement WifiConfiguration for WPA2

--- src/fr/utc/nf28/moka/DeviceConfigurationActivity.java
+++ src/fr/utc/nf28/moka/DeviceConfigurationActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -42,6 +43,12 @@ public class DeviceConfigurationActivity extends Activity {
 	 */
 	private WifiManager mWifiManager;
 
+	/**
+	 * wireless configuration from tag in extra
+	 */
+	private String mSSID;
+	private String mPWD;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -50,12 +57,15 @@ public class DeviceConfigurationActivity extends Activity {
 		Intent i = getIntent();
 
 		if (i != null && i.hasExtra(EXTRA_SSID) && i.hasExtra(EXTRA_PWD) && i.hasExtra(EXTRA_IP) && i.hasExtra(EXTRA_PORT)) {
-			((TextView) findViewById(R.id.textSSID)).setText(i.getStringExtra(EXTRA_SSID));
-			((TextView) findViewById(R.id.textPWD)).setText(i.getStringExtra(EXTRA_PWD));
+			mSSID = i.getStringExtra(EXTRA_SSID);
+			mPWD = i.getStringExtra(EXTRA_PWD);
+			((TextView) findViewById(R.id.textSSID)).setText(mSSID);
+			((TextView) findViewById(R.id.textPWD)).setText(mPWD);
 			((TextView) findViewById(R.id.textIP)).setText(i.getStringExtra(EXTRA_IP));
 			((TextView) findViewById(R.id.textPort)).setText(i.getStringExtra(EXTRA_PORT));
 		}
-
+		Log.i(TAG, "activity start with ssid = " + mSSID + " and pwd = " + mPWD);
+		enableWifi();
 
 	}
 
@@ -74,6 +84,9 @@ public class DeviceConfigurationActivity extends Activity {
 		mWifiManager.setWifiEnabled(true);
 	}
 
+	/**
+	 * Use to receive broadcast from wifi and network
+	 */
 	private BroadcastReceiver MokaWifiStateChangedReceiver = new BroadcastReceiver() {
 		@Override
 		public void onReceive(Context context, Intent intent) {
@@ -89,10 +102,12 @@ public class DeviceConfigurationActivity extends Activity {
 					break;
 				case WifiManager.WIFI_STATE_ENABLED:
 					//wifi Enable
-					//TODO configure Wifi here
+					Log.i(TAG, "WIFI_STATE_ENABLED");
+					configureWifi();
 					break;
 				case WifiManager.WIFI_STATE_ENABLING:
 					//wifi Enabling
+					Log.i(TAG, "WIFI_STATE_ENABLING");
 					break;
 				case WifiManager.WIFI_STATE_UNKNOWN:
 					break;
@@ -102,10 +117,49 @@ public class DeviceConfigurationActivity extends Activity {
 			if (action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
 				NetworkInfo info = (NetworkInfo) intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
 				if (info.getState().equals(NetworkInfo.State.CONNECTED)) {
-					//TODO start activity caus know, this device is connected
+					Log.i(TAG, "NetworkInfo.State.CONNECTED");
+					launchMainActivity();
+				} else {
+					Log.i(TAG, info.getState().toString());
 				}
 			}
 		}
 	};
 
+	/**
+	 * use to configure wireless connection from code
+	 */
+	private void configureWifi() {
+		Log.i(TAG, "configureWifi");
+		WifiConfiguration mWifiConfig = new WifiConfiguration();
+		mWifiConfig.SSID = "\"" + mSSID + "\"";
+		mWifiConfig.priority = 40;
+		mWifiConfig.preSharedKey = "\"" + mPWD + "\"";
+		mWifiConfig.status = WifiConfiguration.Status.ENABLED;
+		mWifiConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+		mWifiConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_EAP);
+		mWifiConfig.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+		mWifiConfig.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+		mWifiConfig.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+		mWifiConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+		mWifiConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+		mWifiConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+		mWifiConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+		int netId = mWifiManager.addNetwork(mWifiConfig);
+		Log.i(TAG, "addNetWork return code : " + String.valueOf(netId));
+		boolean b = mWifiManager.enableNetwork(netId, true);
+		Log.i(TAG, "enableNetwork return code : " + String.valueOf(b));
+
+	}
+
+	private void launchMainActivity() {
+		startActivity(new Intent(DeviceConfigurationActivity.this, MainActivity.class));
+		finish();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		unregisterReceiver(MokaWifiStateChangedReceiver);
+	}
 }
