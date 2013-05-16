315771a19b0ec70372e820fa4112fe6d37c34ade
Thomas <thomas.keunebroek@gmail.com>
2013-05-16 20:10:47 +0200

    Code cleanup

--- src/fr/utc/nf28/moka/ManualConfigurationActivity.java
+++ src/fr/utc/nf28/moka/ManualConfigurationActivity.java
@@ -1,5 +1,6 @@
 package fr.utc.nf28.moka;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
@@ -10,20 +11,17 @@ import android.nfc.Tag;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
-import de.keyboardsurfer.android.widget.crouton.Crouton;
-import fr.utc.nf28.moka.ui.ConfigurationFragment;
-import fr.utc.nf28.moka.util.CroutonUtils;
-import fr.utc.nf28.moka.util.LogUtils;
-import fr.utc.nf28.moka.util.NfcUtils;
 
 import java.io.UnsupportedEncodingException;
 
-public class ManualConfigurationActivity extends Activity {
-	/**
-	 * Tag for log cat
-	 */
-	private static final String TAG = LogUtils.makeLogTag(ManualConfigurationActivity.class);
+import de.keyboardsurfer.android.widget.crouton.Crouton;
+import fr.utc.nf28.moka.ui.ConfigurationFragment;
+import fr.utc.nf28.moka.util.CroutonUtils;
+import fr.utc.nf28.moka.util.NfcUtils;
 
+import static fr.utc.nf28.moka.util.LogUtils.makeLogTag;
+
+public class ManualConfigurationActivity extends Activity {
 	/**
 	 * key for connection configs in sharedPreferences
 	 */
@@ -31,21 +29,27 @@ public class ManualConfigurationActivity extends Activity {
 	public static final String KEY_PREF_PWD = "preference_wifi_password";
 	public static final String KEY_PREF_IP = "preference_jade_ip";
 	public static final String KEY_PREF_PORT = "preference_jade_port";
-
 	/**
-	 * use to available nfc
+	 * Tag for log cat
 	 */
-	private NfcAdapter mNfcAdapter;
-
+	private static final String TAG = makeLogTag(ManualConfigurationActivity.class);
 	/**
 	 * scheme for tag
 	 */
 	private static final String TAG_MOKA_SCHEME = "http://moka.fr/c/?";
+	/**
+	 * use to available nfc
+	 */
+	private NfcAdapter mNfcAdapter;
+	private SharedPreferences mPrefs;
 
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.manual_configuration_activity);
+
+		mPrefs = PreferenceManager.getDefaultSharedPreferences(ManualConfigurationActivity.this);
 
 		//display fragment as main content
 		getFragmentManager().beginTransaction().add(R.id.settings, new ConfigurationFragment()).commit();
@@ -53,12 +57,11 @@ public class ManualConfigurationActivity extends Activity {
 		findViewById(R.id.connect).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(ManualConfigurationActivity.this);
-				Intent i = new Intent(ManualConfigurationActivity.this, DeviceConfigurationActivity.class);
-				i.putExtra(DeviceConfigurationActivity.EXTRA_SSID, sharedPref.getString(KEY_PREF_SSID, ""));
-				i.putExtra(DeviceConfigurationActivity.EXTRA_PWD, sharedPref.getString(KEY_PREF_PWD, ""));
-				i.putExtra(DeviceConfigurationActivity.EXTRA_IP, sharedPref.getString(KEY_PREF_IP, ""));
-				i.putExtra(DeviceConfigurationActivity.EXTRA_PORT, sharedPref.getString(KEY_PREF_PORT, ""));
+				final Intent i = new Intent(ManualConfigurationActivity.this, DeviceConfigurationActivity.class);
+				i.putExtra(DeviceConfigurationActivity.EXTRA_SSID, mPrefs.getString(KEY_PREF_SSID, ""));
+				i.putExtra(DeviceConfigurationActivity.EXTRA_PWD, mPrefs.getString(KEY_PREF_PWD, ""));
+				i.putExtra(DeviceConfigurationActivity.EXTRA_IP, mPrefs.getString(KEY_PREF_IP, ""));
+				i.putExtra(DeviceConfigurationActivity.EXTRA_PORT, mPrefs.getString(KEY_PREF_PORT, ""));
 				startActivity(i);
 			}
 		});
@@ -83,14 +86,11 @@ public class ManualConfigurationActivity extends Activity {
 	 * let the activity to claim priority on tag_discover event when
 	 * the activity is displayed
 	 */
-
 	private void enableNfcDiscovering() {
-		if (mNfcAdapter != null) {
-			PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
-					new Intent(this, this.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-			IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
-			mNfcAdapter.enableForegroundDispatch(this, pendingIntent, new IntentFilter[]{tagDetected}, null);
-		}
+		final PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+				new Intent(this, this.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+		final IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+		mNfcAdapter.enableForegroundDispatch(this, pendingIntent, new IntentFilter[]{tagDetected}, null);
 	}
 
 	/**
@@ -105,6 +105,7 @@ public class ManualConfigurationActivity extends Activity {
 	@Override
 	protected void onPause() {
 		super.onPause();
+
 		disableNfcDiscovering();
 	}
 
@@ -113,12 +114,11 @@ public class ManualConfigurationActivity extends Activity {
 		super.onNewIntent(intent);
 		if (intent.hasExtra(NfcAdapter.EXTRA_TAG)) {
 			try {
-				SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(ManualConfigurationActivity.this);
 				NfcUtils.writeMokaTag((Tag) intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
-						,TAG_MOKA_SCHEME+sharedPref.getString(KEY_PREF_SSID, "")+","
-								+sharedPref.getString(KEY_PREF_PWD, "")+","
-								+sharedPref.getString(KEY_PREF_IP, "")+","
-								+sharedPref.getString(KEY_PREF_PORT, "")
+						, TAG_MOKA_SCHEME + mPrefs.getString(KEY_PREF_SSID, "") + ","
+						+ mPrefs.getString(KEY_PREF_PWD, "") + ","
+						+ mPrefs.getString(KEY_PREF_IP, "") + ","
+						+ mPrefs.getString(KEY_PREF_PORT, "")
 						, false);
 			} catch (UnsupportedEncodingException e) {
 				e.printStackTrace();
