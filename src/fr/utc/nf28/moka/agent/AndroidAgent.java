ca27c19c177797aa233cbdaed1d85d973ef1b0ae
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-05-19 15:31:01 +0200

    Register AndrodAgent to the DF

--- src/fr/utc/nf28/moka/agent/AndroidAgent.java
+++ src/fr/utc/nf28/moka/agent/AndroidAgent.java
@@ -1,5 +1,6 @@
 package fr.utc.nf28.moka.agent;
 
+import fr.utc.nf28.moka.util.JadeUtils;
 import jade.core.Agent;
 
 public class AndroidAgent extends BaseAgent implements IAndroidAgent {
@@ -7,6 +8,7 @@ public class AndroidAgent extends BaseAgent implements IAndroidAgent {
 	@Override
 	protected void setup() {
 		super.setup();
+		registerSkill(JadeUtils.JADE_SKILL_NAME_ANDROID);
 	}
 
 	@Override
