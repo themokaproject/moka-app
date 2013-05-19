cdef7d18d4772b2af0cf083057f4bf01279ddeca
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-05-19 13:23:58 +0200

    Add BasicAgent from moka-server

---
+++ src/fr/utc/nf28/moka/agent/BaseAgent.java
@@ -0,0 +1,88 @@
+package fr.utc.nf28.moka.agent;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+
+import java.util.ArrayList;
+
+/**
+ * A base agent that can register skills
+ * and retrieve agents based on their skills
+ */
+public class BaseAgent extends Agent {
+
+	private static final String DEFAULT_TYPE = "DefaultServiceType";
+
+	/**
+	 * Retrieve all the agents AID that have a skill
+	 * that matches the name
+	 *
+	 * @param skillName
+	 * @return an arrayList of AIDs
+	 */
+	protected  ArrayList<AID> getAgentsWithSkill(String skillName) {
+		return getAgentsWithSkill(skillName, DEFAULT_TYPE);
+	}
+
+	/**
+	 * Retrieve all the agents AID that have a skill
+	 * that matches the name and the type
+	 *
+	 * @param skillName
+	 * @param skillType
+	 * @return an arrayList of AIDs
+	 */
+	protected ArrayList<AID> getAgentsWithSkill(String skillName, String skillType) {
+		ArrayList<AID> result = new ArrayList<AID>();
+		try {
+			DFAgentDescription[] agentDescriptions = DFService.search(this, getAgentDescriptionWithService(skillName, skillType));
+			for(DFAgentDescription ad : agentDescriptions ) {
+				result.add(ad.getName());
+			}
+		} catch (FIPAException e) {
+			e.printStackTrace();
+		}
+
+		return result;
+
+	}
+
+	/**
+	 * Register a skill with a name
+	 *
+	 * @param skillName
+	 */
+	protected void registerSkill(String skillName) {
+		//use a default type
+		// type is a mandatory field of a service-description
+		registerSkill(skillName, DEFAULT_TYPE);
+	}
+
+	/**
+	 * Register a skill with a name and a type
+	 *
+	 * @param skillName
+	 * @param skillType
+	 */
+	protected void registerSkill(String skillName, String skillType) {
+		try {
+			DFService.register(this, getAgentDescriptionWithService(skillName, skillType));
+		} catch (FIPAException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private DFAgentDescription getAgentDescriptionWithService(String skillName, String skillType) {
+		DFAgentDescription agentDescription = new DFAgentDescription();
+		ServiceDescription serviceDescription = new ServiceDescription();
+		serviceDescription.setName(skillName);
+		serviceDescription.setType(skillType);
+		agentDescription.addServices(serviceDescription);
+		return  agentDescription;
+	}
+
+}
