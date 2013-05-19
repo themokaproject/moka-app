1d56a51dceb48015043f9fef2fabdbed11135e16
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-05-19 15:00:26 +0200

    Add Skill name and type in JadeUtils

--- src/fr/utc/nf28/moka/util/JadeUtils.java
+++ src/fr/utc/nf28/moka/util/JadeUtils.java
@@ -8,8 +8,16 @@ import jade.wrapper.ControllerException;
 
 public class JadeUtils {
 
+	/**
+	 * Agent name for AndroidAgent. Unique name based on UUID
+	 */
 	public static final String ANDROID_AGENT_NICKNAME = "AndroidAgent_" + UUID.randomUUID().toString();
 
+	/**
+	 * use to call method of AndroidAgent from Activity
+	 *
+	 * @return interface reference of AndroidAgent running on device
+	 */
 	public IAndroidAgent getAndroidAgentInterface() {
 		try {
 			return MicroRuntime.getAgent(ANDROID_AGENT_NICKNAME).getO2AInterface(IAndroidAgent.class);
@@ -18,4 +26,14 @@ public class JadeUtils {
 			return null;
 		}
 	}
+
+	/**
+	 * default type for Jade skill registering
+	 */
+	public static final String JADE_SKILL_TYPE_DEFAULT = "MokaDefaultSkillType";
+
+	/**
+	 * name for Jade skill registering of AndroidAgent
+	 */
+	public static final String JADE_SKILL_NAME_ANDROID = "AndroidAgentSkillName";
 }
