2c1c8fe38518c23522dbbc486a55a6d20858fe4d
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-05-22 22:17:06 +0200

    Remove useless Import

--- src/fr/utc/nf28/moka/ui/settings/IpAdressPreference.java
+++ src/fr/utc/nf28/moka/ui/settings/IpAdressPreference.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.preference.EditTextPreference;
 import android.util.AttributeSet;
-import android.util.Log;
 
 import de.keyboardsurfer.android.widget.crouton.Crouton;
 import de.keyboardsurfer.android.widget.crouton.Style;
