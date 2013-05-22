6501b9e7efd498e8f6e013689f2d774bca1cd118
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-05-22 19:54:01 +0200

    Try to implement regex validator for ip address

--- src/fr/utc/nf28/moka/util/RegexUtils.java
+++ src/fr/utc/nf28/moka/util/RegexUtils.java
@@ -1,4 +1,22 @@
 package fr.utc.nf28.moka.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexUtils {
+
+	private static final String REGEX_IP_ADDRESS =
+			"^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+					"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+					"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+					"([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+
+	private static Pattern mPattern;
+	private static Matcher mMatcher;
+
+	public static boolean validateIpAddress(String ip){
+		mPattern = Pattern.compile(REGEX_IP_ADDRESS);
+		mMatcher = mPattern.matcher(ip);
+		return mMatcher.matches();
+	}
 }
