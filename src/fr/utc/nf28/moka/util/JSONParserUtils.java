bbb19bf49b61195f6020cb761e6aaf3bf79e143d
Thomas Barthélémy <thomas.barthelemy.utc@gmail.com>
2013-05-23 08:58:37 +0200

    Create JSONParserUtils on utils server model

---
+++ src/fr/utc/nf28/moka/util/JSONParserUtils.java
@@ -0,0 +1,18 @@
+package fr.utc.nf28.moka.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import fr.utc.nf28.moka.agent.A2ATransaction;
+
+/**
+ * a JSON serializer/deserializer that uses Jackson
+ */
+public class JSONParserUtils {
+    private static final ObjectMapper sMapper = new ObjectMapper();
+
+	public String serializeA2ATransaction(A2ATransaction transaction) throws JsonProcessingException{
+		return sMapper.writeValueAsString(transaction);
+	}
+
+}
