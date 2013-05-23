fb1977d65db41cddd18d05a397ff571e9f8aab2c
Thomas <thomas.keunebroek@gmail.com>
2013-05-23 12:29:12 +0200

    Clean RegexUtils code

--- src/fr/utc/nf28/moka/util/RegexUtils.java
+++ src/fr/utc/nf28/moka/util/RegexUtils.java
@@ -1,22 +1,16 @@
 package fr.utc.nf28.moka.util;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexUtils {
-
 	private static final String REGEX_IP_ADDRESS =
 			"^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
 					"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
 					"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
 					"([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+	private static final Pattern sIpPattern = Pattern.compile(REGEX_IP_ADDRESS);
 
-	private static Pattern mPattern;
-	private static Matcher mMatcher;
-
-	public static boolean validateIpAddress(String ip){
-		mPattern = Pattern.compile(REGEX_IP_ADDRESS);
-		mMatcher = mPattern.matcher(ip);
-		return mMatcher.matches();
+	public static boolean validateIpAddress(String ip) {
+		return sIpPattern.matcher(ip).matches();
 	}
 }
