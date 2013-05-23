12a5168721d043f14c803b0178be0297230402ab
Thomas <thomas.keunebroek@gmail.com>
2013-05-23 16:37:07 +0200

    Return a dummy agent interface in case of ControllerException to avoid NPE in any case

--- src/fr/utc/nf28/moka/util/JadeUtils.java
+++ src/fr/utc/nf28/moka/util/JadeUtils.java
@@ -7,11 +7,43 @@ import jade.core.MicroRuntime;
 import jade.wrapper.ControllerException;
 
 public class JadeUtils {
-
 	/**
 	 * Agent name for AndroidAgent. Unique name based on UUID
 	 */
 	public static final String ANDROID_AGENT_NICKNAME = "AndroidAgent_" + UUID.randomUUID().toString();
+	/**
+	 * default type for Jade skill registering
+	 */
+	public static final String JADE_SKILL_TYPE_DEFAULT = "MokaDefaultSkillType";
+	/**
+	 * name for Jade skill registering of AndroidAgent
+	 */
+	public static final String JADE_SKILL_NAME_ANDROID = "AndroidAgentSkillName";
+	/**
+	 * name for Jade skill registering of ConnectionAgent
+	 */
+	public static final String JADE_SKILL_NAME_CONNECTION = "ConnectionAgentSkillName";
+	/**
+	 * name for Jade skill registering of CreationAgent
+	 */
+	public static final String JADE_SKILL_NAME_CREATION = "CreationAgentSkillName";
+	private static final IAndroidAgent sDummyAndroidAgentInterface = new IAndroidAgent() {
+		@Override
+		public void connectPlatform(String firstName, String lastName, String ip) {
+		}
+
+		@Override
+		public void createItem() {
+		}
+
+		@Override
+		public void lockItem() {
+		}
+
+		@Override
+		public void editItem() {
+		}
+	};
 
 	/**
 	 * use to call method of AndroidAgent from Activity
@@ -23,27 +55,7 @@ public class JadeUtils {
 			return MicroRuntime.getAgent(ANDROID_AGENT_NICKNAME).getO2AInterface(IAndroidAgent.class);
 		} catch (ControllerException e) {
 			e.printStackTrace();
-			return null;
+			return sDummyAndroidAgentInterface;
 		}
 	}
-
-	/**
-	 * default type for Jade skill registering
-	 */
-	public static final String JADE_SKILL_TYPE_DEFAULT = "MokaDefaultSkillType";
-
-	/**
-	 * name for Jade skill registering of AndroidAgent
-	 */
-	public static final String JADE_SKILL_NAME_ANDROID = "AndroidAgentSkillName";
-
-	/**
-	 * name for Jade skill registering of ConnectionAgent
-	 */
-	public static final String JADE_SKILL_NAME_CONNECTION = "ConnectionAgentSkillName";
-
-	/**
-	 * name for Jade skill registering of CreationAgent
-	 */
-	public static final String JADE_SKILL_NAME_CREATION = "CreationAgentSkillName";
 }
