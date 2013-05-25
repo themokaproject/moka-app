85d99ed93831acabc2acbdd731da81133b9744ca
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-05-25 18:55:47 +0200

    Add contentView

--- src/fr/utc/nf28/moka/WelcomeActivity.java
+++ src/fr/utc/nf28/moka/WelcomeActivity.java
@@ -1,6 +1,13 @@
 package fr.utc.nf28.moka;
 
+import android.os.Bundle;
+
 import com.actionbarsherlock.app.SherlockActivity;
 
 public class WelcomeActivity extends SherlockActivity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.welcome_activity);
+	}
 }
