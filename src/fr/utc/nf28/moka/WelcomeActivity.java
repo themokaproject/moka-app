506cbbe4349cf16f62f6fe7c0f0f32c6f2f71f70
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-05-25 19:45:00 +0200

    Save first name in WelcomeActivity

--- src/fr/utc/nf28/moka/WelcomeActivity.java
+++ src/fr/utc/nf28/moka/WelcomeActivity.java
@@ -1,23 +1,57 @@
 package fr.utc.nf28.moka;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 
 import com.actionbarsherlock.app.SherlockActivity;
 
+import de.keyboardsurfer.android.widget.crouton.Crouton;
 import fr.utc.nf28.moka.ui.nfc.NfcActivity;
+import fr.utc.nf28.moka.util.CroutonUtils;
+import fr.utc.nf28.moka.util.SharedPreferencesUtils;
 
 public class WelcomeActivity extends SherlockActivity {
+
+	private SharedPreferences mPrefs;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.welcome_activity);
 
+		mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
 		findViewById(R.id.welcome_start_moka).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
 				startActivity(new Intent(WelcomeActivity.this, NfcActivity.class));
+			}
+		});
+
+
+		((EditText)findViewById(R.id.welcome_first_name)).addTextChangedListener(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+			}
+
+			@Override
+			public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+			}
+
+			@Override
+			public void afterTextChanged(Editable editable) {
+				mPrefs.edit().putString(SharedPreferencesUtils.KEY_PREF_FIRST_NAME,editable.toString()).commit();
+				Crouton.makeText(WelcomeActivity.this
+						, "prénom enregistré : "+editable.toString()
+						, CroutonUtils.INFO_MOKA_STYLE).show();
 			}
 		});
 	}
