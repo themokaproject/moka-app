97c6d9005b867378c65ba7756700e0adbeb23a74
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-05-25 19:34:31 +0200

    Implement Moka Access from WelcomActivity

--- src/fr/utc/nf28/moka/WelcomeActivity.java
+++ src/fr/utc/nf28/moka/WelcomeActivity.java
@@ -1,13 +1,24 @@
 package fr.utc.nf28.moka;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.actionbarsherlock.app.SherlockActivity;
 
-public class WelcomeActivity extends SherlockActivity{
+import fr.utc.nf28.moka.ui.nfc.NfcActivity;
+
+public class WelcomeActivity extends SherlockActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.welcome_activity);
+
+		findViewById(R.id.welcome_start_moka).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				startActivity(new Intent(WelcomeActivity.this, NfcActivity.class));
+			}
+		});
 	}
 }
