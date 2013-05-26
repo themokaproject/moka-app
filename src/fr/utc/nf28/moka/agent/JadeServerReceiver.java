86b7b79665f7566c4e3e8539040618e2c2a6db6d
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-05-26 15:52:37 +0200

    Add id for broadcasting message

--- src/fr/utc/nf28/moka/agent/JadeServerReceiver.java
+++ src/fr/utc/nf28/moka/agent/JadeServerReceiver.java
@@ -5,6 +5,12 @@ import android.content.Context;
 import android.content.Intent;
 
 public class JadeServerReceiver extends BroadcastReceiver {
+
+	/**
+	 * identify broadcast message
+	 */
+	public static final String INTENT_FILTER_JADE_SERVER_RECEIVER = "fr.utc.nf28.moka.agent.JadeServerReceiver";
+
 	@Override
 	public void onReceive(Context context, Intent intent) {
 
