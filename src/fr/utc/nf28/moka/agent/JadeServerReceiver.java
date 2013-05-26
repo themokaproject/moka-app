f285f99989289aa63e4eb22897bf650951ea2fae
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-05-26 15:48:56 +0200

    Add JadeServerReceiver

---
+++ src/fr/utc/nf28/moka/agent/JadeServerReceiver.java
@@ -0,0 +1,12 @@
+package fr.utc.nf28.moka.agent;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class JadeServerReceiver extends BroadcastReceiver {
+	@Override
+	public void onReceive(Context context, Intent intent) {
+
+	}
+}
