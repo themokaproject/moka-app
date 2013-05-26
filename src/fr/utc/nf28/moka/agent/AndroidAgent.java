ee0a71990c658297c15bbc0a1d720d964d6a5a58
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-05-26 09:52:12 +0200

    addBehaviour to AndroidAgent

--- src/fr/utc/nf28/moka/agent/AndroidAgent.java
+++ src/fr/utc/nf28/moka/agent/AndroidAgent.java
@@ -24,6 +24,8 @@ public class AndroidAgent extends BaseAgent implements IAndroidAgent {
 
 		registerSkill(JadeUtils.JADE_SKILL_NAME_ANDROID);
 		registerO2AInterface(IAndroidAgent.class, this);
+
+		addBehaviour(new ReceptionBehaviour());
 	}
 
 	@Override
