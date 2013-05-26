cf1f6a9ea7ada86dc18a7795a55fe960d3ce8f36
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-05-26 09:51:11 +0200

    Implement dumy ReceptionBehaviour for test purpose

--- src/fr/utc/nf28/moka/agent/ReceptionBehaviour.java
+++ src/fr/utc/nf28/moka/agent/ReceptionBehaviour.java
@@ -1,4 +1,28 @@
 package fr.utc.nf28.moka.agent;
 
-public class ReceptionBehaviour {
+import android.util.Log;
+
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+import static fr.utc.nf28.moka.util.LogUtils.makeLogTag;
+
+public class ReceptionBehaviour extends CyclicBehaviour {
+
+	/**
+	 * LogCat tag
+	 */
+	private static final String TAG = makeLogTag(ReceptionBehaviour.class);
+
+	@Override
+	public void action() {
+		final ACLMessage message = myAgent.receive(MessageTemplate.MatchPerformative(ACLMessage.INFORM));
+		if (message != null) {
+			final String content = message.getContent();
+			Log.i(TAG, content);
+		} else {
+			block();
+		}
+	}
 }
