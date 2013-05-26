a294458c14640caa0bb947adccfc61ba17a3061a
Thomas <thomas.keunebroek@gmail.com>
2013-05-27 01:25:14 +0200

    Coding style is now more consistent accross the app

--- src/fr/utc/nf28/moka/agent/A2ATransaction.java
+++ src/fr/utc/nf28/moka/agent/A2ATransaction.java
@@ -8,36 +8,36 @@ public class A2ATransaction {
 	private Object mContent;
 	private String mContentClass;
 
-	public A2ATransaction(){
+	public A2ATransaction() {
 	}
 
 	public A2ATransaction(String type, Object content) {
-		this.mType = type;
-		this.mContent = content;
-		this.mContentClass = content.getClass().getName();
+		mType = type;
+		mContent = content;
+		mContentClass = content.getClass().getName();
 	}
 
-	public void setType(String type){
-		this.mType = type;
-	}
-
-	public String getType(){
+	public String getType() {
 		return mType;
 	}
 
-	public void setContent(Object content){
-		this.mContent = content;
+	public void setType(String type) {
+		mType = type;
 	}
 
-	public Object getContent(){
+	public Object getContent() {
 		return mContent;
 	}
 
-	public void setContentClass(String contentClass) {
-		this.mContentClass = contentClass;
+	public void setContent(Object content) {
+		mContent = content;
 	}
 
 	public String getContentClass() {
 		return mContentClass;
+	}
+
+	public void setContentClass(String contentClass) {
+		mContentClass = contentClass;
 	}
 }
