fdf0dedfbbbb8905dbd6c8f9ca942026c5f29e88
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-05-26 15:30:26 +0200

    Restore Cyclic behavior instead of Behavior which done() returns always false

--- src/fr/utc/nf28/moka/agent/ReceptionBehaviour.java
+++ src/fr/utc/nf28/moka/agent/ReceptionBehaviour.java
@@ -3,12 +3,13 @@ package fr.utc.nf28.moka.agent;
 import android.util.Log;
 
 import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 
 import static fr.utc.nf28.moka.util.LogUtils.makeLogTag;
 
-public class ReceptionBehaviour extends Behaviour {
+public class ReceptionBehaviour extends CyclicBehaviour {
 
 	/**
 	 * LogCat tag
@@ -24,10 +25,5 @@ public class ReceptionBehaviour extends Behaviour {
 		} else {
 			block();
 		}
-	}
-
-	@Override
-	public boolean done() {
-		return false;
 	}
 }
