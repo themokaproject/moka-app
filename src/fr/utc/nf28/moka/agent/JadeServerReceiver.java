4d8ad106b3285edfdd2478aa14f03b31a7760e04
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-05-26 16:00:46 +0200

    Implements basic method of a BroadcastReceiver

--- src/fr/utc/nf28/moka/agent/JadeServerReceiver.java
+++ src/fr/utc/nf28/moka/agent/JadeServerReceiver.java
@@ -11,8 +11,28 @@ public class JadeServerReceiver extends BroadcastReceiver {
 	 */
 	public static final String INTENT_FILTER_JADE_SERVER_RECEIVER = "fr.utc.nf28.moka.agent.JadeServerReceiver";
 
+	/**
+	 * intent extra for callback content
+	 */
+	public static final String EXTRA_JADE_SERVER_MESSAGE = "callbackContentFromJadeServer";
+
+	private IJadeServerReceiver mInterface;
+
+	/**
+	 * constructor
+	 * @param i interface to getCallback
+	 */
+	public JadeServerReceiver(IJadeServerReceiver i) {
+		super();
+		mInterface = i;
+	}
+
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		if (intent.hasExtra(EXTRA_JADE_SERVER_MESSAGE)) {
+			//TODO check if it's a item creation or another callback
+			mInterface.onNewItem();
+		}
 
 	}
 }
