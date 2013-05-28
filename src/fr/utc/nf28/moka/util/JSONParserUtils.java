40680f77f32c8c6c74d1e507abb37a9d88ff61da
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-05-28 17:09:22 +0200

    Add A2ATransaction deserialization in Android JSONParserUtils

--- src/fr/utc/nf28/moka/util/JSONParserUtils.java
+++ src/fr/utc/nf28/moka/util/JSONParserUtils.java
@@ -1,7 +1,9 @@
 package fr.utc.nf28.moka.util;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 
 import fr.utc.nf28.moka.io.agent.A2ATransaction;
 
@@ -11,8 +13,29 @@ import fr.utc.nf28.moka.io.agent.A2ATransaction;
 public class JSONParserUtils {
 	private static final ObjectMapper sMapper = new ObjectMapper();
 
-	public static String serializeA2ATransaction(A2ATransaction transaction) throws JsonProcessingException {
+	public static String serializeA2ATransaction(final A2ATransaction transaction) throws IOException {
 		return sMapper.writeValueAsString(transaction);
+	}
+
+	public static A2ATransaction deserializeA2ATransaction(final String json) throws IOException {
+		final JsonNode rootNode = sMapper.readTree(json);
+		final JsonNode typeNode = rootNode.get("type");
+		final JsonNode contentNode = rootNode.get("content");
+		final JsonNode contentClassNode = rootNode.get("contentClass");
+
+		if (typeNode != null && contentNode != null && contentClassNode != null) {
+			Class contentClass = Object.class;
+			try {
+				contentClass = Class.forName(contentClassNode.asText());
+			} catch (ClassNotFoundException e) {
+				System.out.println("deserializeA2ATransaction:ClassNotFound : " + contentClassNode.asText());
+				System.out.println("use " + contentClass.toString() + " instead");
+			}
+			Object content = sMapper.treeToValue(contentNode, contentClass);
+			return new A2ATransaction(typeNode.asText(), content);
+		}
+
+		return null;
 	}
 
 }
