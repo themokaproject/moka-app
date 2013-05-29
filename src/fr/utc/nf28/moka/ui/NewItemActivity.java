cad4d4f4f2791544419c3b1fe72913cd24622d53
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-05-29 17:16:44 +0200

    Implement IJAdeServerReceiver in NEwItemActivity to enable edition onItemCreationSuccess

--- src/fr/utc/nf28/moka/ui/NewItemActivity.java
+++ src/fr/utc/nf28/moka/ui/NewItemActivity.java
@@ -5,18 +5,20 @@ import android.os.Bundle;
 import com.actionbarsherlock.app.ActionBar;
 
 import de.keyboardsurfer.android.widget.crouton.Crouton;
+import de.keyboardsurfer.android.widget.crouton.Style;
 import fr.utc.nf28.moka.R;
 import fr.utc.nf28.moka.data.ComputerType;
 import fr.utc.nf28.moka.data.MokaType;
 import fr.utc.nf28.moka.data.TextType;
 import fr.utc.nf28.moka.io.agent.IAndroidAgent;
+import fr.utc.nf28.moka.io.agent.IJadeServerReceiver;
 import fr.utc.nf28.moka.ui.base.MokaUpActivity;
 import fr.utc.nf28.moka.util.CroutonUtils;
 import fr.utc.nf28.moka.util.JadeUtils;
 
 import static fr.utc.nf28.moka.util.LogUtils.makeLogTag;
 
-public class NewItemActivity extends MokaUpActivity {
+public class NewItemActivity extends MokaUpActivity implements IJadeServerReceiver{
 	public static final String ARG_TYPE = "arg_type";
 	private static final String TAG = makeLogTag(NewItemActivity.class);
 
@@ -52,5 +54,18 @@ public class NewItemActivity extends MokaUpActivity {
 						CroutonUtils.INFO_MOKA_STYLE).show();
 			}
 		}
+	}
+
+
+	@Override
+	public void onItemCreationSuccess(int id) {
+		Crouton.makeText(this, "id from server :" + String.valueOf(id)+". Ready for edition.",
+				Style.CONFIRM).show();
+		//TODO enable edition
+	}
+
+	@Override
+	public void onNewItemCreated() {
+
 	}
 }
