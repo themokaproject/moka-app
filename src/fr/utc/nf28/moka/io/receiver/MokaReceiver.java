4e8110babbc6548676ae375527909db82abbf96c
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-05-30 17:11:39 +0200

    Create abstract MokaReceiver

---
+++ src/fr/utc/nf28/moka/io/receiver/MokaReceiver.java
@@ -0,0 +1,14 @@
+package fr.utc.nf28.moka.io.receiver;
+
+import android.content.BroadcastReceiver;
+
+public abstract class MokaReceiver extends BroadcastReceiver {
+	/**
+	 * identify broadcast message
+	 */
+	public static final String INTENT_FILTER_JADE_SERVER_RECEIVER = "fr.utc.nf28.moka.io.agent.JadeServerReceiver";
+	/**
+	 * intent extra for callback content
+	 */
+	public static final String EXTRA_JADE_REQUEST = "requestFromSma";
+}
