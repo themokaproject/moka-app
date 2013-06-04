acc6398d9493f2fbf31e42a4f6e6d5e1a90ab80a
Thomas Barthélémy <thomasbarthelemyutc@gmail.com>
2013-06-04 22:44:07 +0200

    Implement Multipart Post with retrofit (mime type doesn't match with jersey ?)

--- src/fr/utc/nf28/moka/io/MokaRestService.java
+++ src/fr/utc/nf28/moka/io/MokaRestService.java
@@ -1,12 +1,16 @@
 package fr.utc.nf28.moka.io;
 
-import java.util.HashMap;
 import java.util.List;
 
 import fr.utc.nf28.moka.data.HistoryEntry;
 import retrofit.Callback;
 import retrofit.client.Response;
 import retrofit.http.GET;
+import retrofit.http.Multipart;
+import retrofit.http.PUT;
+import retrofit.http.Part;
+import retrofit.http.Path;
+import retrofit.mime.TypedFile;
 
 public interface MokaRestService {
 	@GET("/history/list")
@@ -14,4 +18,9 @@ public interface MokaRestService {
 
 	@GET("/item/list")
 	void itemsEntries(Callback<Response> cb);
+
+
+	@PUT("/image/{id}")
+	@Multipart
+	void uploadPicture(@Path("id") int itemId, @Part("picture") TypedFile picture, Callback<Response> cb);
 }
--- src/fr/utc/nf28/moka/ui/UploadTestActivity.java
+++ src/fr/utc/nf28/moka/ui/UploadTestActivity.java
@@ -5,6 +5,7 @@ import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
@@ -20,21 +21,33 @@ import de.keyboardsurfer.android.widget.crouton.Style;
 import fr.utc.nf28.moka.R;
 import fr.utc.nf28.moka.data.MediaType;
 import fr.utc.nf28.moka.data.MokaItem;
+import fr.utc.nf28.moka.io.MokaRestService;
 import fr.utc.nf28.moka.io.receiver.CreationReceiver;
 import fr.utc.nf28.moka.io.receiver.MokaReceiver;
 import fr.utc.nf28.moka.util.CroutonUtils;
 import fr.utc.nf28.moka.util.JadeUtils;
+import fr.utc.nf28.moka.util.MokaRestHelper;
+import fr.utc.nf28.moka.util.SharedPreferencesUtils;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import retrofit.mime.TypedFile;
 
-public class UploadTestActivity extends SherlockActivity implements CreationReceiver.OnCreationCallbackListener {
+public class UploadTestActivity extends SherlockActivity implements CreationReceiver.OnCreationCallbackListener, Callback<Response> {
 
 	private Button mUpload;
 	private CreationReceiver mJadeCreationReceiver;
 	private final IntentFilter mIntentFilter = new IntentFilter(MokaReceiver.INTENT_FILTER_JADE_SERVER_RECEIVER);
+	private int mItemId;
 
 	//Capture stuff
 	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 	private Uri fileUri;
 	private static final String TEMP_IMAGE_NAME = "uploadTemp.jpg";
+
+	//upload stuff
+	private MokaRestService mMokaRestService;
+	private static final String DEFAULT_REST_SERVER_IP = "192.168.1.6";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -47,13 +60,19 @@ public class UploadTestActivity extends SherlockActivity implements CreationRece
 			@Override
 			public void onClick(View view) {
 				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-				fileUri = UploadTestActivity.this.getOutputMediaFileUri();
+				fileUri = UploadTestActivity.this.getTempPictureFileUri();
 				intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
 				startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
 			}
 		});
+		final String API_URL = "http://" + PreferenceManager.getDefaultSharedPreferences(this)
+				.getString(SharedPreferencesUtils.KEY_PREF_IP, DEFAULT_REST_SERVER_IP) + "/api";
+		mMokaRestService = MokaRestHelper.getMokaRestService(API_URL);
 
 		mJadeCreationReceiver = new CreationReceiver(this);
+
+		//send creation request to the SMA
+		JadeUtils.getAndroidAgentInterface().createItem(MediaType.ImageType.KEY_TYPE);
 
 	}
 
@@ -63,6 +82,7 @@ public class UploadTestActivity extends SherlockActivity implements CreationRece
 			if (resultCode == RESULT_OK) {
 				Crouton.makeText(this, "image saved",
 						Style.CONFIRM).show();
+				mMokaRestService.uploadPicture(mItemId, new TypedFile("image/jpeg", getTempPictureFile()), this);
 
 			} else if (resultCode == RESULT_CANCELED) {
 				Crouton.makeText(this, "image capture canceled",
@@ -84,12 +104,11 @@ public class UploadTestActivity extends SherlockActivity implements CreationRece
 	protected void onResume() {
 		super.onResume();
 		LocalBroadcastManager.getInstance(this).registerReceiver(mJadeCreationReceiver, mIntentFilter);
-		//send creation request to the SMA
-		JadeUtils.getAndroidAgentInterface().createItem(MediaType.ImageType.KEY_TYPE);
 	}
 
 	@Override
 	public void onSuccess(MokaItem newItem) {
+		mItemId = newItem.getId();
 		Crouton.makeText(this, "id from server :" + String.valueOf(newItem.getId()) + ". Ready for editing.",
 				Style.CONFIRM).show();
 		mUpload.setEnabled(true);
@@ -104,14 +123,14 @@ public class UploadTestActivity extends SherlockActivity implements CreationRece
 	/**
 	 * Create a file Uri for saving our temp image
 	 */
-	private static Uri getOutputMediaFileUri() {
-		return Uri.fromFile(getOutputMediaFile());
+	private static Uri getTempPictureFileUri() {
+		return Uri.fromFile(getTempPictureFile());
 	}
 
 	/**
 	 * Create a File for saving our temp image
 	 */
-	private static File getOutputMediaFile() {
+	private static File getTempPictureFile() {
 
 		final File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
 				Environment.DIRECTORY_PICTURES), "Moka");
@@ -128,4 +147,15 @@ public class UploadTestActivity extends SherlockActivity implements CreationRece
 		final File mediaFile = new File(mediaStorageDir.getPath() + File.separator + TEMP_IMAGE_NAME);
 		return mediaFile;
 	}
+
+	@Override
+	public void success(Response response, Response response2) {
+		Log.d("DEBUG", "retrofit success ");
+	}
+
+	@Override
+	public void failure(RetrofitError retrofitError) {
+		Log.d("DEBUG", "retrofit failed ");
+	}
+
 }
